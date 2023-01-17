157729d13fb868da04f5e367c28ee553f857a260
Mark Rattle <mark.rattle@gmail.com>
2023-01-16 21:33:19 -0500

    tried a few different methods

--- Code/decorators.py
+++ Code/decorators.py
@@ -10,14 +10,7 @@ def runtime_calc(func):
         step_time_end = datetime.now()
         step_time_interval = step_time_end - step_time_start
         s = step_time_interval.seconds
-        ms = f'00{int(step_time_interval.microseconds / 1000)}'[-3:]
+        ms = f'{int(step_time_interval.microseconds) / 1000000}'
         step_time_string = f'{s % 60}.{ms}'
-        print(f"Runtime for this iteration: {step_time_string} seconds")
-    return wrapper
-
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
+        print(f"Total runtime for {func}: {ms} seconds\n")
     return wrapper
--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,33 +1,62 @@
+import timeit
 import sys
 import random
+# import mmap
 from decorators import runtime_calc
+
+num_words = int(sys.argv[1])
+words = []
+
+def read_dictionary():
+    start = timeit.default_timer()
+    with open("/usr/share/dict/words") as text:
+        words = text.read().split()
+    end = timeit.default_timer()
+    print(f'Time to read words: {(end - start):.6f} seconds')  
+    return words
+
+# def read_dictionary():
+#     start = timeit.default_timer()
+#     with open("/usr/share/dict/words", 'rb') as f:
+#         mmapped_file = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+#         words = mmapped_file.read().decode().split('\n')
+#     end = timeit.default_timer()
+#     print(f'Time to read words: {end - start} seconds')  
+#     return words
 
 @runtime_calc
 def random_sentence():
-    num_words = int(sys.argv[1])
-
-    infile = open("/usr/share/dict/words", 'r')
-    text = infile.readlines()
-    infile.close()
-    words = []
-
-    for line in text:
-        word = line.strip("\n")
-        words.append(word)
-
+    words = read_dictionary()
     sentence = ""
-
+    chosen_words = []
     for _ in range(num_words):
         random_word = random.choice(words)
-        while "'" in random_word:
-            random_word = random.choice(words)
-        sentence += random_word
-        random_word = ""
-        if _ < num_words - 1:
-            sentence += " "
-    sentence += "."
-    print(sentence)
-    return sentence
+        chosen_words.append(random_word)    
+    return print(f'{" ".join(chosen_words)}.')
+
+@runtime_calc
+def random_sentence_2():
+    words = read_dictionary()
+    sentence = ""
+    index_list = []
+    chosen_words = []
+
+    for _ in range(num_words):
+        index_list.append(random.randint(0,len(words)-1))
+    index_list.sort()
+
+    for index in index_list:
+        chosen_words.append(words[index])
+    return print(f'{" ".join(chosen_words)}.')
+
+@runtime_calc
+def random_sentence_3():
+    words = read_dictionary()
+    random_words = random.sample(words, k=num_words)
+    return print(f"{' '.join(random_words)}.")
 
 if __name__ == "__main__":
+
     random_sentence()
+    random_sentence_2()
+    random_sentence_3()
