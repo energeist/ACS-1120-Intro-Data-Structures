206caba0eab5e4feb11aef58fe9132df60213a04
Mark Rattle <mark.rattle@gmail.com>
2023-01-18 16:03:59 -0500

    added substring matching

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -5,7 +5,19 @@ import random
 from decorators import runtime_calc
 
 num_words = int(sys.argv[1])
-words = []
+autocomplete_string = ""
+
+if len(sys.argv) > 2:
+    autocomplete_string = str(sys.argv[2])
+    words = []
+
+# def read_dictionary():
+#     start = timeit.default_timer()
+#     with open("/usr/share/dict/words") as text:
+#         words = text.read().split()
+#     end = timeit.default_timer()
+#     print(f'Time to read words: {(end - start):.6f} seconds')  
+#     return words
 
 def read_dictionary():
     start = timeit.default_timer()
@@ -14,6 +26,19 @@ def read_dictionary():
     end = timeit.default_timer()
     print(f'Time to read words: {(end - start):.6f} seconds')  
     return words
+
+# def read_dictionary():
+#     start = timeit.default_timer()
+#     with open("/usr/share/dict/words") as text:
+#         print(len(text.read()))
+#         start2 = timeit.default_timer()
+#         print(len(text.readlines()))
+#         end2 = timeit.default_timer()
+#         print(f'Time to read words: {(end2 - start2):.6f} seconds')  
+#         words = text.read().split()
+#     end = timeit.default_timer()
+#     print(f'Time to read words: {(end - start):.6f} seconds')  
+#     return words
 
 # def read_dictionary():
 #     start = timeit.default_timer()
@@ -26,7 +51,7 @@ def read_dictionary():
 
 @runtime_calc
 def random_sentence():
-    words = read_dictionary()
+    # words = read_dictionary()
     sentence = ""
     chosen_words = []
     for _ in range(num_words):
@@ -36,27 +61,47 @@ def random_sentence():
 
 @runtime_calc
 def random_sentence_2():
-    words = read_dictionary()
+    # words = read_dictionary()
     sentence = ""
     index_list = []
     chosen_words = []
 
     for _ in range(num_words):
         index_list.append(random.randint(0,len(words)-1))
-    index_list.sort()
+    # index_list.sort()
 
     for index in index_list:
         chosen_words.append(words[index])
     return print(f'{" ".join(chosen_words)}.')
 
+potential_words = []
+start_with_string_words = []
+def auto_complete():
+    with open("/usr/share/dict/words") as text:
+        words = text.readlines()
+    print(len(words))
+    for word in words:
+        if autocomplete_string.lower() in word.strip("\n").lower():
+            potential_words.append(word.strip("\n"))
+        if autocomplete_string.lower() == word.strip("\n").lower()[:len(autocomplete_string)]:
+            start_with_string_words.append(word)
+        
+    if len(potential_words) == 0:
+        return print("There are no words that match that string")
+    else:
+        return print(f"There are {len(potential_words)} words that contain the substring '{autocomplete_string}'. Of those words, {len(start_with_string_words)} of them begin with '{autocomplete_string}'. The list begins with {potential_words[0]} and ends with {potential_words[-1]}.")
+
 @runtime_calc
 def random_sentence_3():
-    words = read_dictionary()
+    # words = read_dictionary()
     random_words = random.sample(words, k=num_words)
     return print(f"{' '.join(random_words)}.")
 
 if __name__ == "__main__":
-
+    words = read_dictionary()
     random_sentence()
     random_sentence_2()
     random_sentence_3()
+    potential_words = []
+    if autocomplete_string:
+        auto_complete()
