e580f6a6c7bf1ee96700c583db3bc20c400581d3
Mark Rattle <mark.rattle@gmail.com>
2023-01-18 19:37:53 -0500

    added random_calc_4

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -9,15 +9,8 @@ autocomplete_string = ""
 
 if len(sys.argv) > 2:
     autocomplete_string = str(sys.argv[2])
-    words = []
 
-# def read_dictionary():
-#     start = timeit.default_timer()
-#     with open("/usr/share/dict/words") as text:
-#         words = text.read().split()
-#     end = timeit.default_timer()
-#     print(f'Time to read words: {(end - start):.6f} seconds')  
-#     return words
+words = []
 
 def read_dictionary():
     start = timeit.default_timer()
@@ -26,19 +19,6 @@ def read_dictionary():
     end = timeit.default_timer()
     print(f'Time to read words: {(end - start):.6f} seconds')  
     return words
-
-# def read_dictionary():
-#     start = timeit.default_timer()
-#     with open("/usr/share/dict/words") as text:
-#         print(len(text.read()))
-#         start2 = timeit.default_timer()
-#         print(len(text.readlines()))
-#         end2 = timeit.default_timer()
-#         print(f'Time to read words: {(end2 - start2):.6f} seconds')  
-#         words = text.read().split()
-#     end = timeit.default_timer()
-#     print(f'Time to read words: {(end - start):.6f} seconds')  
-#     return words
 
 # def read_dictionary():
 #     start = timeit.default_timer()
@@ -76,6 +56,26 @@ def random_sentence_2():
 
 potential_words = []
 start_with_string_words = []
+
+@runtime_calc
+def random_sentence_3():
+    # words = read_dictionary()
+    random_words = random.sample(words, k=num_words)
+    return print(f"{' '.join(random_words)}.")
+
+@runtime_calc
+def random_sentence_4():
+    word_list = []
+    index_list = []
+
+    for _ in range(int(sys.argv[1])):
+        index_list.append(random.randint(0,102401))
+    infile = open("/usr/share/dict/words","r")
+
+    for index in index_list:
+        word_list.append(infile.readline(index))
+    infile.close()
+
 def auto_complete():
     with open("/usr/share/dict/words") as text:
         words = text.readlines()
@@ -91,17 +91,13 @@ def auto_complete():
     else:
         return print(f"There are {len(potential_words)} words that contain the substring '{autocomplete_string}'. Of those words, {len(start_with_string_words)} of them begin with '{autocomplete_string}'. The list begins with {potential_words[0]} and ends with {potential_words[-1]}.")
 
-@runtime_calc
-def random_sentence_3():
-    # words = read_dictionary()
-    random_words = random.sample(words, k=num_words)
-    return print(f"{' '.join(random_words)}.")
 
 if __name__ == "__main__":
     words = read_dictionary()
     random_sentence()
     random_sentence_2()
     random_sentence_3()
+    random_sentence_4()
     potential_words = []
     if autocomplete_string:
         auto_complete()
